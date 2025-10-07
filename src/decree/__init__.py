b11872f40129af50256bc20f95b3650745a98192
Steven Cutting <techcommunity@scutting.com>
2025-10-06 20:52:33 -0700

    Reorder __all__ exports in __init__.py for consistency

--- src/decree/__init__.py
+++ src/decree/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["AdrStatus", "AdrRecord", "AdrRef", "LinkSpec", "AdrLog"]
+__all__ = ["AdrLog", "AdrRecord", "AdrRef", "AdrStatus", "LinkSpec"]
 __version__ = "0.1.0"
 
 from .core import AdrLog
